6dd2228d9b33b1257c235258b25ec2e7c7c38cb7
MohamedFarid648 <mohamedfarid648@gmail.com>
2021-01-07 21:57:27 +0200

    feat:Create User Design 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-//Small template design from : https://stackoverflow.com/questions/43200545/create-a-responsive-toolbar-using-angular-material
+/*Small template design from : 
+https://stackoverflow.com/questions/43200545/create-a-responsive-toolbar-using-angular-material */
 export class AppComponent {
   title = 'req-users';
 }
